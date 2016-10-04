204657628b186393d625b4afda7b2cf177574136
black11pearl <bibbin4u@gmail.com>
2016-10-04 12:41:26 +0530

    Removed ACC Controller Authorize

--- MinusBugs.Institute.Web.FrontEnd/Controllers/AccountController.cs
+++ MinusBugs.Institute.Web.FrontEnd/Controllers/AccountController.cs
@@ -12,7 +12,7 @@ using MinusBugs.Institute.Web.FrontEnd.Models;
 
 namespace MinusBugs.Institute.Web.FrontEnd.Controllers
 {
-    [Authorize]
+    
     public class AccountController : Controller
     {
         private ApplicationSignInManager _signInManager;
